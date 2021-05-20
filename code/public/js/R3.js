16870dcbf524ed5724d1a631f9d014421d893554
joaquin_eas@hotmail.com <joaquin_eas@hotmail.com>
2021-05-20 16:53:19 -0300

    R2 casi finalizado

--- code/public/js/R3.js
+++ code/public/js/R3.js
@@ -3,9 +3,9 @@ const modal = document.querySelector(".modal")
 const close = document.querySelector(".close")
 
 open.addEventListener("click", function (){
-    return modal.classList.add("show")
+    modal.classList.add("show")
 })
 
 close.addEventListener("click", function (){
-    return modal.classList.remove("show")
+     modal.classList.remove("show")
 })
